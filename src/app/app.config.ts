6cdf3de5f72b9cbc1f32e7f9b0eec1d8f47d6e6f
Michal <michalmalara@gmail.com>
2026-01-18 09:00:45 +0100

    Add SVG icon support for d10 and update related components; enhance icon styling and loading

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,9 @@
-import {ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
+import {APP_INITIALIZER, ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
 import {provideRouter} from '@angular/router';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {MatIconRegistry} from '@angular/material/icon';
+import {DomSanitizer} from '@angular/platform-browser';
 
 import {routes} from './app.routes';
 import {caseConverterInterceptor} from './shared/http/case-converter.interceptor';
@@ -12,5 +14,13 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptors([caseConverterInterceptor])),
     provideAnimationsAsync(),
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      useFactory: (iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) => () => {
+        iconRegistry.addSvgIcon('d10', sanitizer.bypassSecurityTrustResourceUrl('/icons/d10-svgrepo-com.svg'));
+      },
+      deps: [MatIconRegistry, DomSanitizer],
+    },
   ],
 };
